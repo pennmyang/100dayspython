cbf7cb4471bb86cf4923cdbb5cb1375b6697c806
pennmyang <mickyang@seas.upenn.edu>
2024-10-05 19:48:11 -0400

    Day 8 - Functions Keyword vs positional arguments

---
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -0,0 +1,9 @@
+# Functions with input
+
+def greet_with_name(name,location):
+    print(f"Hello {name}")
+    print(f"what is it like in {location}?")
+
+
+# greet_with_name("Jack Bauer","macdonalds")
+greet_with_name(location="maccy Ds",name="Jack Bauer")
